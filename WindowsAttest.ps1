cd3529afc6b0b72953aa15b540b2bbeb95e0913c
vinfnet <29166491+vinfnet@users.noreply.github.com>
2024-11-19 16:27:37 +0000

    v2 script, faster with better error handling for vc redist install

--- WindowsAttest.ps1
+++ WindowsAttest.ps1
@@ -1,22 +1,30 @@
-#Simple script to do an in-guest attestation in a Confidential VM - https://aka.ms/accdocs
+#.WinGuestAttestation.ps1 v2
+# Simon Gallagher, Microsoft https://github.com/vinfnet
+# NO warranties implied, use at your own risk
+#Script to run on a Confidential Virtual machine (https://aka.ms/accdocs) to check if it is running on a Confidential VM (CVM) and attested by Azure Attestation service
 #More detailed version https://github.com/Azure/confidential-computing-cvm-guest-attestation/tree/main/cvm-platform-checker-exe
-#and you'll need something to decode the JSON like https://jsonformatter.org/json-parser - or code below uses a community module to decode it
-#TODO replace the AttestationClientApp.exe and do the call inside the PowerShell script rather than using an external (albeit open source) binary
-#No warranty, use at your own risk, etc.
-# Requires access to the Internet to download the attestation client, attest and install a PowerShell module
+#it will download a sample app from GitHub to get the JWT token from the attestation service and decode it
+#it will install + uninstall dependencies like VC Redist and JWTDetails (https://github.com/darrenjrobinson/JWTDetails) PowerShell module
 
-#PowerShell module required to decode the JWT token from the attestation service https://github.com/darrenjrobinson/JWTDetails
-install-module -name JWTDetails
-
+write-output "Installing JWTDetails module"
+install-module -name JWTDetails -Force
+write-output "Downloading attestation client binaries"
 Invoke-WebRequest -uri https://github.com/Azure/confidential-computing-cvm-guest-attestation/raw/main/cvm-platform-checker-exe/Windows/cvm_windows_attestation_client.zip -OutFile windowsattestationclient.zip
 Expand-Archive -Path .\windowsattestationclient.zip -DestinationPath .
 cd .\cvm_windows_attestation_client
-# need to install the VC++ redistributable 1st - do silent install
-.\VC_redist.x64.exe /q /norestart
-start-sleep -Seconds 15 # wait for install to finish - hacky but works
-#TODO loop to check for vcredist registry key
 
-#get the JWT output 
+write-output "Installing VC redistributable"
+$vcProcess = Start-Process -FilePath ".\VC_redist.x64.exe" -ArgumentList "/install /passive /norestart" -Wait -PassThru
+
+# Check the exit code of the process
+if ($vcProcess.ExitCode -ne 0) {
+    Write-Host "Error: VC redistributable installation failed with exit code $($vcProcess.ExitCode)" -ForegroundColor Red
+    exit $vcProcess.ExitCode
+} else {
+    Write-Host "VC redistributable installed successfully."
+}
+
+#get the JWT output from the attestation service
 $attestationJWT = .\AttestationClientApp.exe -a "sharedweu.weu.attest.azure.net" -n "12345" -o token
 $attestationJSON = Get-JWTDetails($attestationJWT)
 Write-Host "This " $attestationJSON."x-ms-azurevm-ostype" " OS is running on " $attestationJSON."x-ms-isolation-tee"."x-ms-attestation-type" "VM hardware"
@@ -28,13 +36,22 @@ if ($attestationJSON."x-ms-isolation-tee"."x-ms-compliance-status" -eq "azure-co
 else {
     Write-Host "This VM is NOT an Azure compliant CVM" -ForegroundColor Red
 }
-# optional - uninstall VC redist and PowerShell module and remove files afterwards
-Write-Host "Cleaning up"
-.\VC_redist.x64.exe /u /q /norestart
-start-sleep -Seconds 15 # wait for install to finish - hacky but works
- #TODO loop to check for vcredist registry key
-Uninstall-module -name JWTDetails
+# optional - uninstall VC redist and PowerShell module afterwards
+write-output "Removing VC redistributable"
+$vcUninstallProcess = Start-Process -FilePath ".\VC_redist.x64.exe" -ArgumentList "/uninstall /passive /norestart" -Wait -PassThru
+
+# Check the exit code of the process
+if ($vcUninstallProcess.ExitCode -ne 0) {
+    Write-Host "Error: VC redistributable uninstallation failed with exit code $($vcUninstallProcess.ExitCode)" -ForegroundColor Red
+    exit $vcUninstallProcess.ExitCode
+} else {
+    Write-Host "VC redistributable uninstalled successfully."
+}
+
+write-output "Removing JWTDetails module"
+Uninstall-module -name JWTDetails -Force
 cd ..
-remove-item -path .\cvm_windows_attestation_client -Recurse
+write-output "Cleaning-up files"
+remove-item -path .\cvm_windows_attestation_client -recurse
 remove-item -path .\windowsattestationclient.zip -Recurse
-Write-Host "Finished"
+write-output "Finished"
